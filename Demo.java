7a12d0b622c6690c8607b8844c7cc846e731f0d5
praveen kumar <praveen kumar@praveenkumar-PC>
2017-09-17 20:25:16 +0530

    [commented lines] test purpose

--- Demo.java
+++ Demo.java
@@ -6,5 +6,14 @@ public static void main(String[] args) {
 	System.out.println("Second commit");
 	System.out.println("third commit");
 	System.out.println("fourth commit");
+	
+	
+/*	System.out.println("Inside main method");
+	System.out.println("First commit");
+	System.out.println("Second commit");
+	System.out.println("third commit");
+	System.out.println("fourth commit");*/
+	
+	
 }
 }
