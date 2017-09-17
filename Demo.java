dff80d5d17c1642aa9cde2e654c7840b286e9aa6
praveen kumar <praveen kumar@praveenkumar-PC>
2017-09-17 20:13:15 +0530

    [second commit] test message

--- Demo.java
+++ Demo.java
@@ -3,5 +3,6 @@ public class Demo {
 public static void main(String[] args) {
 	System.out.println("Inside main method");
 	System.out.println("First commit");
+	System.out.println("Second commit");
 }
 }
