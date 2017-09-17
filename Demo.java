69ece2057297d0a262e73569b2332282141727c4
praveen kumar <praveen kumar@praveenkumar-PC>
2017-09-17 20:23:47 +0530

    [print] fourth message

--- Demo.java
+++ Demo.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("First commit");
 	System.out.println("Second commit");
 	System.out.println("third commit");
+	System.out.println("fourth commit");
 }
 }
